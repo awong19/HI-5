fdd80ffa0504a2b4084048ebd1b88cb842603c47
js <vsegav12@gmail.com>
2019-04-08 19:12:32 +0100

    improving the page
    Fixed the jquery a bit to create the navbar. added melis to the team.

--- html/abouts.js
+++ html/abouts.js
@@ -1,7 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script type="text/javascript">
+
 $(document).ready(function () {
 $('.navbar-light .dmenu').hover(function () {
         $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
@@ -9,4 +6,3 @@ $('.navbar-light .dmenu').hover(function () {
         $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
     });
 });
-</script>
